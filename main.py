01fb801d1359f8ac77ed677b7573f49c4502656c
Azu0925 <yamaneko564783@gmail.com>
2020-07-14 15:18:21 +0900

    ディレクトリチェック

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-print("switchのディレクトリを入力してください。")
-dir = input()
+import os
+import sys
+import glob
+
+def check_dir(dir):
+    if not os.path.exists(dir):
+        print("ディレクトリが存在しません。終了するにはなにかキーを入力してください。")
+        input()
+        sys.exit()
+    print('ディレクトリの存在を確認しました。')
+
+
+print("switchのディレクトリを確認します。。。")
+dir = "F:\\Nintendo\\Album"
+check_dir(dir)
 
 print("保存先のディレクトリを入力してください。")
 save_dir = input()
-
+check_dir(save_dir)
